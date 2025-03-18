85559784bb5a196b9afb7cd459ec75f7a700aee9
Asher Drybrough <asher_iz@outlook.com>
2025-03-18 08:59:37 -0700

    Updated version for github

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,6 @@ function insertNameFromFirestore() {
         }
     })
 }
-insertNameFromFirestore();
 // Function to read the quote of the day from the Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
 function readQuote(day) {
@@ -54,4 +53,93 @@ function readQuote(day) {
             console.log ("Error calling onSnapshot", error);
         });
     }
- readQuote("Tuesday");        //calling the function
+
+ //-----------------------------------------------
+// Create a "max" number of hike document objects
+//-----------------------------------------------
+function writeHikeLoop(max) {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+    var imgs = ["NV01",
+                "AM01",
+                "BBY01"]
+    for (i = 1; i <= max; i++) {
+        let imgIndex = (i - 1) % imgs.length;
+        hikesRef.add({ //add to database, autogen ID
+            code: imgs[imgIndex],
+            name: "hike" + i,
+            details: "Elmo says this hike is amazing!  You will love going on hike" + i,
+            lat: 49+i,    //randomly different
+            lng: -122+i,  //randomly different
+            last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        })
+   }
+}
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allHikes=> {
+            var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allHikes.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;       // get value of the "name" key
+                var details = doc.data().details;  // get value of the "details" key
+				var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var hikeLength = doc.data().length; //gets the length field
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID="+docID;
+
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                i++;   //Optional: iterate variable to serve as unique ID
+                console.log(i);
+            })
+        })
+}
+
+// displayCardsDynamically("hikes");  //input param is the name of the collection
+
+//Global variable pointing to the current user's Firestore document
+var currentUser;   
+
+//Function that calls everything needed for the main page  
+function doAll() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
+            console.log(currentUser);
+
+            // figure out what day of the week it is today
+            const weekday = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+            const d = new Date();
+            let day = weekday[d.getDay()];
+
+            // the following functions are always called when someone is logged in
+            readQuote(day);
+            console.log(day);
+            insertNameFromFirestore();
+            displayCardsDynamically("hikes");
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+            window.location.href = "login.html";
+        }
+    });
+}
+doAll();
